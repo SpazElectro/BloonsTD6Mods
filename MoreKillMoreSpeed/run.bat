60d1dddf6d458069e9e933a28fcd0ebb4ba13f95
StevenRafft <steven.rafft@gmail.com>
2022-03-01 15:45:34 +0200

    commit

--- MoreKillMoreSpeed/run.bat
+++ MoreKillMoreSpeed/run.bat
@@ -4,8 +4,8 @@ echo Building...
 dotnet build
 
 echo Moving...
-move "D:\AAAASteven\btd6 modding\mods\MoreKillMoreSpeed\bin\Debug\net4.7.2\MoreKillMoreSpeed.dll" "D:\SteamLibrary\steamapps\common\BloonsTD6\Mods"
-move "D:\AAAASteven\btd6 modding\mods\MoreKillMoreSpeed\bin\Debug\net4.7.2\MoreKillMoreSpeed.pdb" "D:\SteamLibrary\steamapps\common\BloonsTD6\Mods"
+move "./bin\Debug\net4.7.2\MoreKillMoreSpeed.dll" "D:\SteamLibrary\steamapps\common\BloonsTD6\Mods"
+move "./bin\Debug\net4.7.2\MoreKillMoreSpeed.pdb" "D:\SteamLibrary\steamapps\common\BloonsTD6\Mods"
 
 echo Starting...
 "D:\SteamLibrary\steamapps\common\BloonsTD6\BloonsTD6.exe"
